6e688899634eef81c158600e3c248aa4adefbe6b
sujipark <charmmming7@gmail.com>
2024-04-09 11:23:46 +0900

    :lipstick: add tailwind style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <div>Home</div>;
+  const products = ['Tomatoes', 'Pasta', 'Coconut'];
+
+  return (
+    <div className="flex justify-center bg-gray-800 p-10">
+      <div className=" text-center text-white">
+        <h1 className="mb-4 text-3xl">상품목록</h1>
+        {products.map((p, i) => {
+          return (
+            <div className="mb-2" key={p.toString()}>
+              <strong className="p-4 pt-2">{p} 40$</strong>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
